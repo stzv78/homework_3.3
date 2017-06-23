008d6a068b70a13cc0e124142cac4368b0649ba2
stzv78 <stzv78@mail.ru>
2017-06-24 00:47:41 +0300

    second

--- hw_3.3/app/classes/cart/Cart.php
+++ hw_3.3/app/classes/cart/Cart.php
@@ -4,14 +4,18 @@ namespace classes\cart;
 class Cart
 { 
   
-  public $products = array();
+    public $products = array();
 
   
-  public function addProduct($product) {
-  $this->products[] = $product;
-  }
+    public function addProduct($product) {
+        foreach ($product  as $key => $value) {
+            foreach ($value as $k => $val) {
+                $this->products[] = $val;
+            }
+        }
+    }
 
-  public function printProduct() {
-    var_dump($this->products);
-  }
+    public function printProduct() {
+        var_dump($this->products);
+  } 
 }
